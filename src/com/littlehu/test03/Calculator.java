c3c93336d6df31f1c7304d78e94af54074bc8f53
blackfox <huangy_tao@126.com>
2018-10-30 15:25:08 +0800

    Lambda表达式作为方法的参数

---
+++ src/com/littlehu/test03/Calculator.java
@@ -0,0 +1,6 @@
+package com.littlehu.test03;
+@FunctionalInterface
+public interface Calculator {
+	int caculate(int x,int y);
+
+}
---
+++ src/com/littlehu/test03/TestMain.java
@@ -0,0 +1,20 @@
+package com.littlehu.test03;
+
+public class TestMain {
+	public static void main(String[] args) {
+		engine((x,y)->x+y);
+		engine((x,y)->x*y);
+		engine((x,y)->x-y);
+		engine((x,y)->x/y);
+		
+	}
+	
+	private static void engine(Calculator calculator){
+		
+		int x=2,y=4;
+		int result = calculator.caculate(x, y);
+		System.out.println("The result is "+result);
+	}
+	
+
+}
