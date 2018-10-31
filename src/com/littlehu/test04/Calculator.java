1e93a6b71b69865a64b8200fe116da7f77d3b085
blackfox <huangy_tao@126.com>
2018-10-31 13:47:09 +0800

    赋值上下文

---
+++ src/com/littlehu/test04/Calculator.java
@@ -0,0 +1,7 @@
+package com.littlehu.test04;
+
+@FunctionalInterface
+public interface Calculator {
+	int calculate(int x,int y);
+
+}
---
+++ src/com/littlehu/test04/TestMain.java
@@ -0,0 +1,9 @@
+package com.littlehu.test04;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Calculator clCalculator=(x,y)->x+y;
+		System.out.println(clCalculator.calculate(1, 2));
+	}
+
+}
