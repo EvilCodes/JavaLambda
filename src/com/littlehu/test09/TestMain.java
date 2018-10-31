d1a7b4c0076360acf6280d7336ff5f12d26a0d8d
blackfox <huangy_tao@126.com>
2018-10-31 15:53:56 +0800

    调用静态方法的另一种形式

--- src/com/littlehu/test09/TestMain.java
+++ src/com/littlehu/test09/TestMain.java
@@ -6,12 +6,17 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		Function<Integer, String> func1=x->Integer.toBinaryString(x);
-		System.out.println(func1.apply(1));
+		System.out.println(func1.apply(12));
 		
 		
 		
 		Function<String, Integer> func2=x->Integer.parseInt(x);
 		System.out.println(func2.apply("345"));
+		
+		
+		
+		Function<Integer, String> func3=Integer::toBinaryString;
+		System.out.println(func3.apply(12));
 		
 		
 	}
